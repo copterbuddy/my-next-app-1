84948e8994adaa9f2c0bf1179759605695ae77f9
Cop.Kunanon <kunanon.l@iconext.co.th>
2024-01-31 11:51:24 +0700

    use tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,35 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <h1>Hello, Next.js!</h1>
+async function getBlogs() {
+  const response = await fetch(
+    "https://65b0d8e5d16d31d11bdd62cc.mockapi.io/api/data/Blogs"
   );
+
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+
+  return response.json();
+}
+
+export default async function Page(/*{ params }: { params: { slug: string } }*/) {
+  const blogs: Blogs[] = await getBlogs();
+
+  return (
+    <div>
+      Test Page
+      {blogs.map((blog: any, index: number) => (
+        <div key={blog.id}>
+          <div>
+            {blog.id} {blog.name} {blog.content}
+            <button className="px-4 bg-blue-400">Go to Read Blog</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+interface Blogs {
+  id: number;
+  name: String;
+  content: String;
 }
